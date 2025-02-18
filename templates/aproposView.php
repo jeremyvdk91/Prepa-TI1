514baf4f03e5187922ebdec572c3195f0cee28fc
jeremyvdk91 <vanderkelen.jer@gmail.com>
2025-02-18 14:54:51 +0100

    commit3

--- templates/aproposView.php
+++ templates/aproposView.php
@@ -3,12 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css" />
     <?php
 include "inc/menuIncView.php";
     ?>
-    <title>Document</title>
+    <title>A propos</title>
 </head>
 <body>
+    <img src="img/itachi.jpg" alt="">
 <footer><?php
 include "inc/footerIncView.php";
        ?></footer>
--- templates/histoireView.php
+++ templates/histoireView.php
@@ -3,12 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css" />
     <?php
 include "inc/menuIncView.php";
     ?>
-    <title>Document</title>
+    <title>Histoire</title>
 </head>
 <body>
+    <img src="img/bird.jpg" alt="">
+    <img src="img/lezard.jpg" alt="">
+    <img src="img/herison.jpg" alt="">
+    
 <footer><?php
 include "inc/footerIncView.php";
        ?></footer>
